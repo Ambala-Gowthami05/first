14ff6937a1c6608078642a4612cf39a1c981bb6d
Ambala-Gowthami05 <gowthamiambala5@gmail.com>
2026-06-06 15:08:49 +0530

    Delete set.py

--- set.py
+++
@@ -1,9 +0,0 @@
-"""n={"gow","mou","niv"}
-print(n)
-n.add("sushu")
-print(n)
-#intersection and difference etc......"""
-n={1,2,3,4,5}
-m={3,4,1,6,7}
-print(n.intersection(m))
-print(m.difference(n))
